e8ecd77f26545341e1d84859f53b0f032525ba5c
Rahul <rahulmanu7902@gmail.com>
2024-08-13 17:21:17 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import SideBar from '@/components/shared/SideBar'
+import React from 'react'
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <main className='root'>
+            <SideBar />
+            <div className='root-container'>
+                <div className='wrapper'>
+                    {children}
+                </div>
+            </div>
+
+        </main>
+    )
+}
+
+export default Layout
